d36117a1d879de2de102f22d6635f8ac08d39ca4
Chris MacLellan <maclellan.christopher@gmail.com>
2014-04-23 13:15:05 -0400

    modified urls so that the root is the sort page.

--- humansort/urls.py
+++ humansort/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^sort/', include('sort.urls')),
+    url(r'^', include('sort.urls')),
     url(r'^images/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root':'images'}),
 )
